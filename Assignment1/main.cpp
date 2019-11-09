f903f191166ae3eacabd948e48ea80d977a7675d
Alexander Dahlin <alexander.dahlins@gmail.com>
2019-11-09 14:02:52 +0100

    Started on PoolAllocator

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <list>
+
+template <typename T>
+class PoolAllocator
+{
+public:
+	using ID = unsigned int;
+
+	PoolAllocator() = default;
+	~PoolAllocator() = default;
+
+	T* Allocate();
+	void Free();
+
+private:
+	std::list<ID> m_FreeList;
+	T* m_pMemory;
+};
+
 
 int main(int argc, const char* argv[])
 {   
