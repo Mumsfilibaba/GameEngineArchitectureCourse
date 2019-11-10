ec2b3d16e81bc70b012be181d5809665f5c08ed8
TimTheToad <tim.b.mellander@gmail.com>
2019-11-10 15:28:27 +0100

    Tim Testar GitDesk
    Hello no one

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, const char* argv[])
 {   
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Hello Another World" << std::endl;
+	std::cout << "Hello no one!" << std::endl;
 	std::cin.get();
     return 0; 
 }
