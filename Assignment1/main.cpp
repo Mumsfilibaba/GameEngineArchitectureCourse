717115953358585c7a7dd56ac0cbaba69cfadeeb
Alexander Dahlin <alexander.dahlins@gmail.com>
2019-11-10 16:57:39 +0100

    Added SFML and ImGui

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 int main(int argc, const char* argv[])
 {   
 	std::cout << "Hello World" << std::endl;
-	std::cout << "Hello Another World" << std::endl;
-	std::cout << "Hello no one!" << std::endl;
-	std::cin.get();
+
+	sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+	window.setFramerateLimit(60);
+	ImGui::SFML::Init(window);
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	sf::Clock deltaClock;
+	while (window.isOpen()) 
+	{	
+		sf::Event event;
+		while (window.pollEvent(event)) 
+		{
+			ImGui::SFML::ProcessEvent(event);
+
+			if (event.type == sf::Event::Closed) 
+			{
+				window.close();
+			}
+		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::ShowTestWindow();
+
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::End();
+
+		window.clear();
+		window.draw(shape);
+		ImGui::SFML::Render(window);
+		window.display();
+	}
+
+	ImGui::SFML::Shutdown();
+
     return 0; 
 }
--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,42 @@ workspace "Game Engine Architecture"
         "Debug",
         "Release"
     }
+	
+	group "Dependencies"
+		include "ImGui"
+	group ""
+	
+	project "*"
+
+	filter "system:windows"
+		sysincludedirs
+		{
+			"Dependencies/SFML-2.5.1/include",
+			"ImGui"
+		}
+		libdirs
+		{
+			"Dependencies/SFML-2.5.1/lib"
+		}
+		links
+		{
+			"opengl32",
+			"freetype",
+			"winmm",
+			"gdi32",
+			"flac",
+			"vorbisenc",
+			"vorbisfile",
+			"vorbis",
+			"ogg",
+			"ws2_32"
+		}
+
+	filter "configurations:*"
+		defines 
+		{ 
+			"SFML_STATIC" 
+		}
 
     filter "configurations:Debug"
         symbols "On"
@@ -18,6 +54,14 @@ workspace "Game Engine Architecture"
         {
             "DEBUG"
         }
+		links
+		{	
+			"sfml-graphics-s-d",
+			"sfml-window-s-d",
+			"sfml-system-s-d",
+			"sfml-audio-s-d",
+			"sfml-network-s-d"
+		}
 
     filter "configurations:Release"
         symbols "On"
@@ -27,6 +71,19 @@ workspace "Game Engine Architecture"
         {
             "NDEBUG"
         }
+		links
+		{	
+			"sfml-graphics-s",
+			"sfml-window-s",
+			"sfml-system-s",
+			"sfml-audio-s",
+			"sfml-network-s"
+		}
+		
+	dependson
+	{
+		"ImGui"
+	}
 
     project "Assign1_Allocator"
         kind "ConsoleApp"
@@ -34,8 +91,14 @@ workspace "Game Engine Architecture"
         location "Assignment1"
         cppdialect "C++17"
         systemversion "latest"
+		
         files
         {
             "Assignment1/**.cpp",
             "Assignment1/**.h"
         }
+		
+		links
+		{
+			"ImGui"
+		}
