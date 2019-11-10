3bdfc0d5552146004d8930db6c99e8678788025c
Alexander Dahlin <alexander.dahlins@gmail.com>
2019-11-10 15:26:24 +0100

    Updated main.cpp

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
-#include <list>
-#include <mutex>
-
-template <typename T>
-class PoolAllocator
-{
-public:
-	using ID = unsigned int;
-
-	PoolAllocator() = default;
-	~PoolAllocator() = default;
-
-	T* Allocate();
-	void Free(T*);
-
-private:
-	std::mutex m_Mutex;
-	std::list<ID> m_FreeList;
-	T* m_pMemory;
-};
-
 
 int main(int argc, const char* argv[])
 {   
 	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello Another World" << std::endl;
 	std::cin.get();
     return 0; 
 }
