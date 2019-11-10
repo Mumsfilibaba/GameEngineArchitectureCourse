21bf238e1baf934a81b82db0b055d58fcb392d22
TimTheToad <tim.b.mellander@gmail.com>
2019-11-10 15:33:03 +0100

    TimTest 2
    TEstar gitIgnore

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, const char* argv[])
 {   
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Hello Another World" << std::endl;
-	std::cout << "Hello no one!" << std::endl;
+	std::cout << "Hello no one again!" << std::endl;
 	std::cin.get();
     return 0; 
 }
