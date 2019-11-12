a4e3a56d513fe71089d1c2efc69345d5253de5db
Alexander Dahlin <alexander.dahlins@gmail.com>
2019-11-12 15:57:07 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -98,7 +98,7 @@ workspace "Game Engine Architecture"
             "NDEBUG"
         }
 		
-	filter { "configurations:release", "system:windows" }
+	filter { "configurations:Release", "system:windows" }
 		links
 		{	
 			"sfml-graphics-s",
