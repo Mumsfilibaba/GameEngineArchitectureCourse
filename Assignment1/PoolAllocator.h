0bb461a622616520b247d4c20488dca8a6a33edc
Alexander Dahlin <alexander@dahlins.nu>
2019-11-15 08:47:22 +0100

    Updated poolallocator, complete with some tests.

---
+++ Assignment1/PoolAllocator.h
@@ -0,0 +1,95 @@
+#ifndef PoolAllocator_h
+#define PoolAllocator_h
+
+#define MB(mb) mb * 1024 * 1024
+
+template<typename T>
+class PoolAllocator
+{
+private:
+    struct Block
+    {
+        Block* pNext = nullptr;
+    };
+
+public:
+    inline PoolAllocator(int sizeInBytes = 4096)
+        : m_pMemory(nullptr),
+        m_pFreeListHead(nullptr),
+        m_SizeInBytes(sizeInBytes)
+    {
+        constexpr int blockSize = std::max(sizeof(T), sizeof(Block));
+        assert(m_SizeInBytes % blockSize == 0);
+        
+        //Allocate mem
+        m_pMemory = aligned_alloc(blockSize, m_SizeInBytes);
+        
+        //Init blocks
+        Block* pOld        = nullptr;
+        Block* pCurrent = (Block*)m_pMemory;
+        m_pFreeListHead = pCurrent;
+
+        int blockCount = m_SizeInBytes / blockSize;
+        for (int i = 0; i < blockCount; i++)
+        {
+            pCurrent->pNext     = (Block*)(((char*)pCurrent) + blockSize); //HACKING;
+            
+            pOld     = pCurrent;
+            pCurrent = pCurrent->pNext;
+        }
+
+        //Set the last valid ptr's next to null
+        pOld->pNext = nullptr;
+    }
+
+    
+    inline ~PoolAllocator()
+    {
+        if (m_pMemory)
+        {
+            free(m_pMemory);
+            m_pMemory = nullptr;
+        }
+    }
+
+
+    inline void* AllocateBlock()
+    {
+        Block* pCurrent = m_pFreeListHead;
+        m_pFreeListHead = m_pFreeListHead->pNext;
+        return (void*)pCurrent;
+    }
+
+
+    template<typename... Args>
+    inline T* MakeNew(Args&& ... args)
+    {
+        return new(AllocateBlock()) T(std::forward<Args>(args) ...);
+    }
+    
+    
+    inline void Free(T* pObject)
+    {
+        Block* pFirst = (Block*)pObject;
+        pFirst->pNext = m_pFreeListHead;
+        m_pFreeListHead = pFirst;
+    }
+    
+    
+    inline int GetBlockSize() const
+    {
+        return sizeof(T);
+    }
+    
+    
+    inline int GetChunkSize() const
+    {
+        return m_SizeInBytes;
+    }
+private:
+    void* m_pMemory;
+    Block* m_pFreeListHead;
+    size_t m_SizeInBytes;
+};
+
+#endif
--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -5,88 +5,64 @@
 #include <SFML/Graphics/CircleShape.hpp>
 #include <imgui.h>
 #include <imgui-SFML.h>
-
-
-class PoolAllocator
-{
-private:
-	struct Block
-	{
-		Block* pNext		= nullptr;
-		Block* pPrevious	= nullptr;
-	};
-
-public:
-	inline PoolAllocator(int startSize, int blockSize)
-		: m_pMemory(malloc(startSize)),
-		m_pFreeListHead(nullptr),
-		m_SizeInBytes(startSize),
-		m_BlockSize(blockSize)
-	{
-		assert(startSize % blockSize == 0);
-		assert(blockSize >= sizeof(Block));
-		
-		Block* pOld		= nullptr;
-		Block* pCurrent = (Block*)m_pMemory;
-		m_pFreeListHead = pCurrent;
-
-		//Init blocks
-		int blockCount = startSize / blockSize;
-		for (int i = 0; i < blockCount; i++)
-		{
-			pCurrent->pNext = (Block*)(((char*)pCurrent) + blockSize); //HACKING;
-			pCurrent->pPrevious = pOld;
-
-			pOld	 = pCurrent;
-			pCurrent = pCurrent->pNext;
-		}
-
-		//Set the last valid ptr's next to null
-		pOld->pNext = nullptr;
-	}
-
-	
-	inline ~PoolAllocator()
-	{
-		if (m_pMemory)
-		{
-			free(m_pMemory);
-			m_pMemory = nullptr;
-		}
-	}
-
-
-	inline void* AllocateBlock()
-	{
-		Block* pBlock = m_pFreeListHead;
-		m_pFreeListHead = m_pFreeListHead->pNext;
-		return (void*)pBlock;
-	}
-
-
-	template<typename T, typename... Args>
-	inline T* MakeNew(Args&& ... args)
-	{
-		assert(m_BlockSize >= sizeof(T));
-		return new(AllocateBlock()) T(std::forward<Args>(args) ...);
-	}
-private:
-	void* m_pMemory;
-	Block* m_pFreeListHead;
-	int m_SizeInBytes;
-	int m_BlockSize;
-};
-
+#include "PoolAllocator.h"
 
 int main(int argc, const char* argv[])
 {
-	PoolAllocator pAllocator(4096, 16);
-	int* pInt1 = pAllocator.MakeNew<int>(1);
-	int* pInt2 = pAllocator.MakeNew<int>(2);
-	int* pInt3 = pAllocator.MakeNew<int>(3);
-	int* pInt4 = pAllocator.MakeNew<int>(4);
-	int* pInt5 = pAllocator.MakeNew<int>(5);
-	int* pInt6 = pAllocator.MakeNew<int>(6);
+	PoolAllocator<int> allocator(MB(2));
+    
+    sf::Clock clock;
+
+    constexpr int count = 4096 * 64;
+    std::cout << "Total memory consumption: " << count * sizeof(long long) << " bytes" << std::endl;
+    int* pPoolAllocated[count];
+    int* pOSAllocated[count];
+    
+    //Allocate from pool
+    clock.restart();
+    sf::Time t1 = clock.getElapsedTime();
+    for (int i = 0; i < count; i++)
+    {
+        pPoolAllocated[i] = allocator.MakeNew(i);
+    }
+    sf::Time t2 = clock.getElapsedTime();
+    
+    std::cout << "Allocating " << count << " vars from pool took: " << (t2-t1).asMicroseconds() << "qs" << std::endl;
+    
+    //Allocate from OS
+    clock.restart();
+    t1 = clock.getElapsedTime();
+    for (int i = 0; i < count; i++)
+    {
+        pOSAllocated[i] = new int(i);
+    }
+    t2 = clock.getElapsedTime();
+    
+    std::cout << "Allocating " << count << " vars from OS took: " << (t2-t1).asMicroseconds() << "qs" << std::endl;
+    
+    //Free to pool
+    clock.restart();
+    t1 = clock.getElapsedTime();
+    for (int i = 0; i < count; i++)
+    {
+        allocator.Free(pPoolAllocated[i]);
+        pPoolAllocated[i] = nullptr;
+    }
+    t2 = clock.getElapsedTime();
+    
+    std::cout << "Freeing " << count << " vars from pool took: " << (t2-t1).asMicroseconds() << "qs" << std::endl;
+    
+    //Free to OS
+    clock.restart();
+    t1 = clock.getElapsedTime();
+    for (int i = 0; i < count; i++)
+    {
+        delete pOSAllocated[i];
+        pOSAllocated[i] = nullptr;
+    }
+    t2 = clock.getElapsedTime();
+    
+    std::cout << "Freeing " << count << " vars from OS took: " << (t2-t1).asMicroseconds() << "qs" << std::endl;
 
 
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Game Engine Architecture");
