ec9ea93c35e7ac898daf7a024593b7c9cab2c3cb
Alexander Dahlin <alexander.dahlins@gmail.com>
2019-12-03 11:12:24 +0100

    woked on collada loader

--- Base/LoaderCOLLADA.cpp
+++ Base/LoaderCOLLADA.cpp
@@ -18,6 +18,39 @@ size_t LoaderCOLLADA::WriteToBuffer(const std::string& file, void* buffer)
 }
 
 
+template<typename T>
+struct TArray
+{
+	std::string ID;
+	size_t Count;
+	std::vector<T> Data;
+};
+using FloatArray = TArray<float>;
+
+
+struct Geometry
+{
+	std::string ID;
+	std::string Name;
+};
+
+
+struct LibraryGeometry
+{
+	std::vector<Geometry> Geometries;
+};
+
+
+enum ETag : uint32_t
+{
+	TAG_UNKNOWN		= 0,
+	TAG_FLOAT_ARRAY = 1
+};
+
+
+ETag SearchTag(const char* iter);
+
+
 std::vector<GameMesh> LoaderCOLLADA::ReadFromDisk(const std::string& filepath)
 {
 	//Read in the full textfile into a buffer
@@ -29,9 +62,47 @@ std::vector<GameMesh> LoaderCOLLADA::ReadFromDisk(const std::string& filepath)
 		return std::vector<GameMesh>();
 	}
 	
+	FloatArray positions;
+	const char* iter = buffer;
+	while (*iter != '\0')
+	{
+		switch (*iter)
+		{
+		case '<':
+		{
+			ETag tag = SearchTag(iter);
+		}
+			break;
+		default:
+			break;
+		}
 
+		iter++;
+	}
 
 	return std::vector<GameMesh>();
 }
 
 
+ETag SearchTag(const char* iter)
+{
+	struct TagPair
+	{
+		ETag Tag			= TAG_UNKNOWN;
+		const char* pTagStr = nullptr;
+	};
+	static const TagPair pairs[] =
+	{
+		{ TAG_FLOAT_ARRAY, "float_array" },
+	};
+
+
+	constexpr size_t numPairs = sizeof(pairs) / sizeof(TagPair);
+	for (size_t i = 0; i < numPairs; i++)
+	{
+		const char* pRes = strstr(iter, pairs[i].pTagStr);
+
+	}
+
+	return TAG_UNKNOWN;
+}
